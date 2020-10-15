5503399df54d6bcae2145c1434292a69adecd272
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-10-15 07:58:04 -0700

    added tests

---
+++ src/ts/index.spec.ts
@@ -0,0 +1,67 @@
+import { EventEmitter } from '@/index';
+import {
+  IEventEmitter,
+  IEmitterCallBack,
+  IEmitterUnsubscribe,
+} from '@/interface';
+
+describe('Checking the methods of the EventEmitter class', () => {
+  let emitter: IEventEmitter;
+
+  beforeEach(() => {
+    emitter = new EventEmitter();
+  });
+
+  describe('method check "emit"', () => {
+    test('the method should return an error', () => {
+      const eventName = 'non-existent event name';
+      try {
+        emitter.emit(eventName);
+      } catch (error) {
+        expect(error.message).toBe(`this event "${eventName}" was not found`);
+      }
+    });
+
+    test('checking the method for performance', () => {
+      const eventName: string = 'test-event-name';
+      const message: string = 'test-message';
+
+      emitter.subscribe(eventName, (text) => {
+        expect(text).toBe(message);
+      });
+
+      emitter.emit(eventName, message);
+    });
+  });
+
+  describe('method check "subscribe"', () => {
+    test('checking the possibility of subscription to events', () => {
+      const eventName: string = 'test-event-name';
+      const listener: IEmitterCallBack = () => {};
+
+      const subscriber = emitter.subscribe(eventName, listener);
+
+      expect(emitter.getListeners()[eventName]).toBeDefined();
+      expect(subscriber).toBeDefined();
+      expect(emitter.getListeners()[eventName][0]).toBe(listener);
+    });
+
+    test('checking the possibility of unsubscribing from events', () => {
+      const eventName: string = 'test-event-name';
+      const listener: IEmitterCallBack = () => {};
+
+      const subscriber: IEmitterUnsubscribe = emitter.subscribe(
+        eventName,
+        listener,
+      );
+      expect(emitter.getListeners()[eventName]).toBeDefined();
+      expect(subscriber).toBeDefined();
+      expect(emitter.getListeners()[eventName][0]).toBe(listener);
+
+      subscriber.unsubscribe();
+
+      expect(emitter.getListeners()[eventName]).toBeDefined();
+      expect(emitter.getListeners()[eventName][0]).toBeUndefined();
+    });
+  });
+});
